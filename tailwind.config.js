84493f73b3314ddc0e63af514ab4e34cc14ad77d
kawtharayomide <kolawolekaosarat08@gmail.com>
2024-08-02 15:35:47 +0100

    well

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,13 @@ module.exports = {
         inter: ["Inter"],
         poppins: ["Poppins"],
         quicksand: ["Quicksand"],
-      }
+      },
+    //   backgroundImage: theme=> ({
+    //     'custom-gradient': 'linear-gradient(130deg, [#0A0D17] 25%, [#19213D] 25%, [#500724] 25%)',
+    // }),
+    
     },
+      
   },
   plugins: [],
 }
