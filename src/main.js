ce9db07dceaef98121de156ef3c033e062d35947
kawtharayomide <kolawolekaosarat08@gmail.com>
2024-08-03 20:19:44 +0100

    commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+
+// document.getElementsByClassName('hamburger').addEventListener('click',function() {
+//     var menu = document.getElementsByid('menu');
+//     menu.classList.toggle('hidden')
+//     if(menu.style.display === 'none' || menu.style.display === '') {
+//         menu.style.display = 'block';
+//     } else {
+//         menu.style.display = 'none';
+//     }
+// });
+// function Menu(e) {
+//     let list = document.querySelector('ul')
+//     e.name === 'union' ? e.name = "" : e.name = "union"
+// }
+
+function onToggleMenu(e){
+    console.log(e)
+}
