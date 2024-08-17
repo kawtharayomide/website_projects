14722578cc5cbd2b331e20a5db89c2f972fd302b
kawtharayomide <kolawolekaosarat08@gmail.com>
2024-08-17 21:40:11 +0100

    HELP!!!!!!

--- src/main.js
+++ src/main.js
@@ -12,7 +12,29 @@
 //     let list = document.querySelector('ul')
 //     e.name === 'union' ? e.name = "" : e.name = "union"
 // }
+// const navLinks = document.querySelector('.nav-Links')
+// function onToggleMenu(e){
+//    e.name = e.name === 'menu' ? 'close' : 'menu'
+//    navLinks.classList.toggle('top-[9%]')
+// }
+const menuBtn = document.querySelector('.menu-btn');
+const drop = document.querySelector('.dropdown');
+let menuOpened = false;
 
-function onToggleMenu(e){
-    console.log(e)
+function openMenu () {
+if (!menuOpened) {
+menuBtn.classList.add('open');
+drop.classList.add('drop');
+menuOpened = true;
+} else {
+menuBtn.classList.remove('open');
+drop.classList.remove('drop');
+menuOpened = false;
+}
+}
+
+function closeMenu () {
+menuBtn.classList.remove('open');
+drop.classList.remove('drop');
+menuOpened = false;
 }
