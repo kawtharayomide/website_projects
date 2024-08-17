c697b02a6dca987f8e255b5ad54d2e97cd91cc93
Ganiyu Idris <Golamide27@gmail.com>
2024-08-17 22:25:33 +0100

    Pushing

--- src/main.js
+++ src/main.js
@@ -17,24 +17,29 @@
 //    e.name = e.name === 'menu' ? 'close' : 'menu'
 //    navLinks.classList.toggle('top-[9%]')
 // }
+
 const menuBtn = document.querySelector('.menu-btn');
 const drop = document.querySelector('.dropdown');
 let menuOpened = false;
 
-function openMenu () {
-if (!menuOpened) {
-menuBtn.classList.add('open');
-drop.classList.add('drop');
-menuOpened = true;
-} else {
-menuBtn.classList.remove('open');
-drop.classList.remove('drop');
-menuOpened = false;
-}
+
+const openMenu = () => {
+	if (!menuOpened) {
+		// console.log('worked')
+		// menuBtn.classList.add('open');
+		console.log(drop);
+	drop.classList.add('open');
+	menuOpened = true;
+	} else {
+		console.log('closed')
+	// menuBtn.classList.remove('open');
+	drop.classList.remove('open');
+	menuOpened = false;
+	}
 }
 
 function closeMenu () {
 menuBtn.classList.remove('open');
-drop.classList.remove('drop');
+drop.classList.remove('open');
 menuOpened = false;
 }
